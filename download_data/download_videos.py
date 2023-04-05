5baac15afa56e8978db9a20ec34098d2eef9cf12
Violet (Ziyu) Xiang <ziyxiang@node05-ccncluster.stanford.edu>
2023-04-05 14:43:32 -0700

    uncommited ancient local changes

--- download_data/download_videos.py
+++ download_data/download_videos.py
@@ -1,3 +1,13 @@
+from __future__ import print_function
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from googleapiclient.http import MediaIoBaseDownload
+
+import io
 import os
 import sys
 import pdb
@@ -7,108 +17,78 @@ import urllib
 import zipfile
 import argparse
 import requests
-import goproplus
-import dropbox
+#sys.path.append('/mnt/fs1/ziyxiang/BabyViewPublic/download_data/')
 
-from tqdm import tqdm
-from shutil import move
-from bs4 import BeautifulSoup
-sys.path.append('/mnt/fs1/ziyxiang/BabyViewPublic/download_data/')
-import access_tokens as at
 
-class GoProDownload:
+# write a function to download videos from google drive
+# 1. get the video id from the google drive link
+# 2. download the video from the video id
+# 3. save the video to the video_root
+class GoogleDriveDownloader:
     def __init__(self, args):
         self.args = args
-        self.gp = goproplus.plus(args.email, args.password)
-        self.access_token = self.gp.getToken()
-        self.headers = {
-            'Accept-Charset': 'utf-8',
-            'Accept': 'application/vnd.gopro.jk.media+json; version=2.0.0',
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer %s' % self.access_token}
+        self.babyview_drive_id = '0AJtfZGZvxvfxUk9PVA'
+        self.SCOPES = ['https://www.googleapis.com/auth/drive']
 
-    def get_video_ids(self):
-        video_ids = []
-        url = "%s/media/search" % self.gp.GOPRO_API_ENDPOINT        
-        resp = requests.get(url, headers=self.headers)                
-        resp.raise_for_status()
-        resp = resp.json()
-        pages = resp['_pages']['total_pages']
-        for page in range(1, pages+1):
-            page_url = f"{url}?page={page}"
-            page_resp = requests.get(page_url, headers=self.headers).json()
-            for id_dict in page_resp['_embedded']['media']:
-                video_ids.append(id_dict['id'])
-        return video_ids
-
+    def download_file(self, service, file_id, file_name, file_path):
+        request = service.files().get_media(fileId=file_id)
+        fh = io.FileIO(file_path, 'wb')
+        downloader = MediaIoBaseDownload(fh, request)
+        done = False
+        while done is False:
+            status, done = downloader.next_chunk()
+            print("Download %d%%." % int(status.progress() * 100))        
+    
     def download_videos(self):
-        video_ids = self.get_video_ids()
-        print(f"Downloading {len(video_ids)} videos")
-        for video_id in tqdm(video_ids):
-            download_url = f"https://api.gopro.com/media/{video_id}/download"
-            try:
-                resp = requests.get(download_url, headers=self.headers)
-                resp.raise_for_status()
-                content = resp.json()
-                filename = content['filename']
-                existing_videos = [
-                    f.split('.')[0] for f in os.listdir(self.args.video_root)
-                    if f.endswith('.MP4') or f.endswith('.zip')]
-                if filename.split('.')[0] not in existing_videos:
-                    video_path = os.path.join(self.args.video_root, filename)
-                    video_url = content['_embedded']['files'][0]['url']
+        creds = None
+        token_path = os.path.join(self.args.cred_folder, 'token.json')
+        if os.path.exists(token_path):
+            creds = Credentials.from_authorized_user_file(token_path, self.SCOPES)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                cred_path = os.path.join(self.args.cred_folder, 'credentials.json')
+                flow = InstalledAppFlow.from_client_secrets_file(cred_path, self.SCOPES)
+                creds = flow.run_local_server(port=self.args.port)
+            # Save the credentials for the next run
+            with open(token_path, 'w') as token:
+                token.write(creds.to_json())
+
+        try:
+            service = build('drive', 'v3', credentials=creds)                 
+            # Call the Drive v3 API
+            results = service.files().list(                
+                driveId=self.babyview_drive_id,
+                corpora='drive',
+                includeItemsFromAllDrives=True,
+                supportsAllDrives=True).execute()
+            items = results.get('files', [])
+            for item in items:
+                if 'mp4' in item['mimeType']:
                     try:
-                        urllib.request.urlretrieve(video_url, video_path)
-                    except:
-                        os.remove(video_path)
-                        print(f'{filename} only has zip, creating new url')
-                        filename = filename.replace('MP4', 'zip')
-                        zip_path = os.path.join(self.args.video_root, filename)
-                        video_url = f"https://api.gopro.com/media/{video_id}/zip/source?access_token={self.access_token}"
-                        command = f'wget {video_url} -O {filename}'                    
-                        try:
-                            os.system(command)
-                            move(filename, zip_path)
-                            with zipfile.ZipFile(zip_path, 'r') as zip_ref:
-                                zip_ref.extractall(self.args.video_root)
-                        except:                            
-                            pass
-            except:
-                pass
-
-
+                        save_path = os.path.join(self.args.video_root, item['name'])
+                        self.download_file(service, item['id'], item['name'], save_path)
+                        print(f"saved item {item['name']} to {save_path}")
+                    except Exception as err:
+                        print(f"An error occured {err}")
+        except HttpError as err:
+            print(f"An error occured {err}")
             
-class DropboxDownload:
-    def __init__(self, args):
-        self.args = args
-        
-    def download_videos(self):
-        print('Authenticating dropbox access..')
-        dbx = dropbox.Dropbox(at.dropbox_accss_token)
-        print('Authentification finished')
-        entries = dbx.files_list_folder('//').entries
-        for entry in entries:
-            download_path = entry.path_lower            
-            vid_path = os.path.join(
-                self.args.video_root, download_path.split('/')[-1])
-            dbx.files_download_to_file(vid_path, download_path)
-        
-        
+
 def main():
-    email = 'email'
-    password = 'password'
-    video_root = 'root_path'
+    video_root = "/data/ziyxiang/Babyview/raw_videos"
+    cred_folder = "/mnt/fs4/ziyxiang/cloud/babyview"
     parser = argparse.ArgumentParser(
         description="Download videos from cloud services")
-    parser.add_argument('--email', type=str, default=email)
-    parser.add_argument('--password', type=str, default=password)
     parser.add_argument('--video_root', type=str, default=video_root)
+    parser.add_argument('--cred_folder', type=str, default=cred_folder)
+    parser.add_argument('--port', type=int, default=8080, help='port number needs to forward from local port')
     args = parser.parse_args()
-    #go_pro = GoProDownload(args)
-    #go_pro.download_videos()
-    dropbox = DropboxDownload(args)
-    dropbox.download_videos()
-    
+    downloader = GoogleDriveDownloader(args)
+    downloader.download_videos()
+
 
 if __name__ == '__main__':
     main()
